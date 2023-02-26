4f75262d63a73326e3100f46ee4a61ccbd13257c
Ben Schwartz <btschwartz12@gmail.com>
2023-02-26 09:30:33 -0500

    Working on projects

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,7 +7,12 @@ import {
 import withRouter from "../hooks/withRouter";
 import AppRoutes from "./routes";
 import Headermain from "../header";
+import AppContext from './AppContext';
+import { ThemeProvider } from 'styled-components';
+import useDarkMode from 'use-dark-mode';
 import AnimatedCursor  from "../hooks/AnimatedCursor";
+import { darkTheme, lightTheme } from '../components/theme/themes';
+import GlobalStyles from '../components/theme/GlobalStyles';
 import "./App.css";
 
 function _ScrollToTop(props) {
@@ -20,22 +25,30 @@ function _ScrollToTop(props) {
 const ScrollToTop = withRouter(_ScrollToTop);
 
 export default function App() {
+
+  const darkMode = useDarkMode(true);
+
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div className="cursor__dot">
-        <AnimatedCursor
-          innerSize={15}
-          outerSize={15}
-          color="255, 255 ,255"
-          outerAlpha={0.4}
-          innerScale={0.7}
-          outerScale={5}
-        />
-      </div>
-      <ScrollToTop>
-        <Headermain />
-        <AppRoutes />
-      </ScrollToTop>
-    </Router>
+    <AppContext.Provider value={{ darkMode }}>
+      <ThemeProvider theme={darkMode.value ? darkTheme : lightTheme}>
+        <GlobalStyles />
+        <Router basename={process.env.PUBLIC_URL}>
+          <div className="cursor__dot">
+            <AnimatedCursor
+              innerSize={15}
+              outerSize={15}
+              color="255, 255 ,255"
+              outerAlpha={0.4}
+              innerScale={0.7}
+              outerScale={5}
+            />
+          </div>
+          <ScrollToTop>
+            <Headermain />
+            <AppRoutes />
+          </ScrollToTop>
+        </Router>
+      </ThemeProvider>
+    </AppContext.Provider>
   );
 }
--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,10 +1,16 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Container, Row, Button } from 'react-bootstrap';
+import { useState, useEffect, useContext } from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import { ThemeContext } from 'styled-components';
 import Fade from 'react-reveal/Fade';
 import endpoints from '../../app/endpoints';
 import ProjectCard from '../../components/projectcard';
-import FallbackSpinner from '../../components/fallbackspinner';
+// import FallbackSpinner from '../../components/fallbackspinner';
+
+
+import React from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { dataportfolio, meta } from "../../content_option";
 
 const styles = {
     containerStyle: {
@@ -15,8 +21,10 @@ const styles = {
     },
 };
 
+
 export const Projects = () => {
     const { bsSecondaryVariant } = useContext(ThemeContext);
+
     const [data, setData] = useState(null);
     const [showMore, setShowMore] = useState(false);
 
@@ -27,39 +35,104 @@ export const Projects = () => {
             .then((res) => res.json())
             .then((res) => setData(res))
             .catch((err) => err);
-        }, []);
+    }, []);
     const numberOfItems = showMore && data ? data.length : 6;
-    console.log('bruh')
-    return (
-        <>        
-            {data
-            ? (
-                <div className="section-content-container">
-                <Container style={styles.containerStyle}>
-                    <Row xs={1} sm={1} md={2} lg={3} className="g-4">
-                    {data.projects?.slice(0, numberOfItems).map((project) => (
-                        <Fade key={project.title}>
-                        <ProjectCard project={project} />
-                        </Fade>
-                    ))}
-                    </Row>
 
-                    {!showMore
-                    && (
-                    <Button
-                        style={styles.showMoreStyle}
-                        variant={bsSecondaryVariant}
-                        onClick={() => setShowMore(true)}
-                    >
-                        show more
-                    </Button>
-                    )}
-                </Container>
+    return (
+        <HelmetProvider>
+        <Container className="About-header">
+            <Helmet>
+            <meta charSet="utf-8" />
+            <title> Projects | {meta.title} </title>{" "}
+            <meta name="description" content={meta.description} />
+            </Helmet>
+            <Row className="mb-5 mt-3 pt-md-3">
+            <Col lg="8">
+                <h1 className="display-4 mb-4"> Projects </h1>{" "}
+                <hr className="t_border my-4 ml-0 text-left" />
+            </Col>
+            </Row>
+            <div className="mb-5 po_items_ho">
+            {/* {dataportfolio.slice(0, numberOfItems).map((proj, i) => (
+                <div key={i} className="po_item">
+                    <img src={proj.img} alt="" />
+                    <div className="content">
+                    <p>{proj.desctiption}</p>
+                    <a href={proj.link}>view project</a>
+                    </div>
                 </div>
-            ) : <FallbackSpinner /> }
-        </>
-        );
-    };
+            ))} */}
+            {data.projects?.slice(0, numberOfItems).map((project) => (
+                <Fade key={project.title}>
+                <ProjectCard project={project} />
+                </Fade>
+            ))}
+
+            </div>
+            <div className='section-content-container'>
+            {!showMore
+            && (
+            <Button
+                style={styles.showMoreStyle}
+                variant={bsSecondaryVariant}
+                onClick={() => setShowMore(true)}
+            >
+                show more
+            </Button>
+            )}
+            </div>
+        </Container>
+        </HelmetProvider>
+    );
+};
+
+
+
+// export const Projects = () => {
+    // const { bsSecondaryVariant } = useContext(ThemeContext);
+    // const [data, setData] = useState(null);
+    // const [showMore, setShowMore] = useState(false);
+
+    // useEffect(() => {
+    //     fetch(endpoints.projects, {
+    //         method: 'GET',
+    //     })
+    //         .then((res) => res.json())
+    //         .then((res) => setData(res))
+    //         .catch((err) => err);
+    //     }, []);
+//     const numberOfItems = showMore && data ? data.length : 6;
+//     console.log('bruh')
+//     return (
+//         <>        
+//             {data
+//             ? (
+//                 <div className="section-content-container">
+//                 <Container style={styles.containerStyle}>
+//                     <Row xs={1} sm={1} md={2} lg={3} className="g-4">
+                    // {data.projects?.slice(0, numberOfItems).map((project) => (
+                    //     <Fade key={project.title}>
+                    //     <ProjectCard project={project} />
+                    //     </Fade>
+                    // ))}
+//                     </Row>
+
+                    // {!showMore
+                    // && (
+                    // <Button
+                    //     style={styles.showMoreStyle}
+                    //     variant={bsSecondaryVariant}
+                    //     onClick={() => setShowMore(true)}
+                    // >
+                    //     show more
+                    // </Button>
+                    // )}
+//                 </Container>
+//                 </div>
+//             ) : <FallbackSpinner /> }
+//         </>
+//         );
+//     };
 
 
 
