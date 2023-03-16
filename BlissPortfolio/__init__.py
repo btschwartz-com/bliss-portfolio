436e41f711ac976f9e4b973d38ef0bb8c94d1ecc
Ben Schwartz <btschwartz12@gmail.com>
2023-03-16 20:20:26 +0000

    Fully working

---
+++ BlissPortfolio/__init__.py
@@ -0,0 +1,38 @@
+import os
+import flask
+from flask_mysqldb import MySQL
+
+app = flask.Flask(__name__)
+
+app.config.from_object('BlissPortfolio.config')
+
+app.public_folder = 'public'
+app.static_folder = 'static'
+
+mysql = MySQL(app)
+
+@app.route('/public/<path:filename>')
+def public_files(filename):
+    return flask.send_from_directory(app.public_folder, filename)
+
+@app.route('/')
+def index():
+    return flask.send_from_directory(app.static_folder, 'index.html')
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def serve_react_app(path):
+    if path != "" and os.path.exists(app.static_folder + "/" + path):
+        return flask.send_from_directory(app.static_folder, path)
+    else:
+        return flask.send_from_directory(app.static_folder, "index.html")
+
+
+@app.route('/piper/dog')
+def a():
+    return "dog"
+
+
+import BlissPortfolio.api
+import BlissPortfolio.model
+import BlissPortfolio.views
